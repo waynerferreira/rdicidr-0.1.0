b650234ca3c66e60ec4496a6bb13985607b897d0
waynerferreira <waynerferreira@hotmail.com>
2026-08-06 00:20:29 -0300

    chore: add terraform files and cd yaml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,9 @@
 # In this file put the variables related to the deployment
-variable "variable_name" {
-    type = "type",
-    description = "Description"
+variable "aws_region" {
+  type    = "type"
+  default = "us-east-1"
+}
+variable "project_name" {
+  type    = string
+  default = "react-app"
 }
